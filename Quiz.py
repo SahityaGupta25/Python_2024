e45f1ac48cd8ac06767151b8c1c7078e76b63870
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-13 11:31:37 +0530

    quiz

--- Quiz.py
+++ Quiz.py
@@ -3,5 +3,27 @@ print("Welcome to the quiz")
 # Input is a pre defined function in python which allows user to take input
 playing = input("Do you want to play (yes/no) - ")
 
-if playing!="yes":
-    quit()
+if playing.lower() != "yes": # .lower() is a method in python which is used to lower the text
+    quit() # Quit is a pre defined function in python used to terminate program
+
+print("Okay! Let's play")
+answer1 = input("Q.1 What does CPU stands for ? ")
+if answer1.lower() == "central processing unit":
+    print("Correct :)")
+else:
+    print("Incorrect!")
+answer1 = input("Q.2 What does GPU stands for ? ")
+if answer1.lower() == "graphics processing unit" or "graphical processing unit":
+    print("Correct :)")
+else:
+    print("Incorrect!")
+answer1 = input("Q.3 What does RAM stands for ? ")
+if answer1.lower() == "random access memory":
+    print("Correct :)")
+else:
+    print("Incorrect!")
+answer1 = input("Q.4 What does PSU stands for ? ")
+if answer1.lower() == "power supply unit":
+    print("Correct :)")
+else:
+    print("Incorrect!")
