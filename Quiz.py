6c16db963aca6baf0a9f03bd5ae1fabc739be11c
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-13 10:41:27 +0530

    quiz

---
+++ Quiz.py
@@ -0,0 +1,7 @@
+print("Welcome to the quiz")
+
+# Input is a pre defined function in python which allows user to take input
+playing = input("Do you want to play (yes/no) - ")
+
+if playing!="yes":
+    quit()
