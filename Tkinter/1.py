1fc597f8e378d49dec91be9d5d7b89089449fd51
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-14 12:16:55 +0530

    files

---
+++ Tkinter/1.py
@@ -0,0 +1,15 @@
+from tkinter import *       #Imports all stuff from tkinter module 
+
+variable_root = Tk()        # This variable is instance of tk class.
+
+#^ GUI logic starting from here
+
+variable_root.mainloop()
+
+
+
+
+
+# STEPS
+# 1. Imported tkinter
+# 2. 
---
+++ Tkinter/2.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+imaginary_tech_root = Tk()
+
+imaginary_tech_root.geometry()          # Width x Height
+
+imaginary_tech_root.mainloop()
