64012d44391c2722bb1bf132a6e3f7fa262b4b45
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-14 13:42:17 +0530

    2

--- Tkinter/2.py
+++ Tkinter/2.py
@@ -2,6 +2,15 @@ from tkinter import *
 
 imaginary_tech_root = Tk()
 
-imaginary_tech_root.geometry()          # Width x Height
+imaginary_tech_root.geometry("460x470")          # imaginary_tech_root.geometry is used foe size of application in " WidthxHeight "
+
+imaginary_tech_root.minsize(200,200)            # Width,Height
+
+imaginary_tech_root.maxsize(800,800)
+
+
+x = Label(text="Mathew is a Good Boy")
+        # Variable
+x.pack()
 
 imaginary_tech_root.mainloop()
