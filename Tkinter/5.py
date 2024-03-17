619848c441fc2cef598b40e3fbac48a009c975de
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-17 15:24:34 +0530

    5py

--- Tkinter/5.py
+++ Tkinter/5.py
@@ -1,18 +1,34 @@
 from tkinter import *
 from tkinter import font
 # Imported font
+from PIL import Image , ImageTk
 root = Tk()
 
 
 
-root.geometry("600x900")
+root.geometry("1260x900")
 
+# ? Min & Max Size      -------------------------------
 root.minsize(400,600)
 
-root.maxsize(1500,700)
+root.maxsize(1600,900)
+# ?------------------------------------------------
+
 
 the = Label(text="THE",font=("Courier New",36,"bold"))
 the.pack()
+
+#^ AI Image     ---------------------------------------------
+ai2_image= PhotoImage(file="./IMG/ai2.png")
+ai2_label = Label(image=ai2_image)
+ai2_label.pack()
+
+
+ai3_image= PhotoImage(file="./IMG/ai3.png")
+ai3_resize = ai3_image.resize((300,200),Image.ANTIALIAS)
+ai3_label = Label(image=ai3_image)
+ai3_label.pack()
+# ^ ----------------------------------------------------------
 
 times = Label(text="TIMES",font=("Courier New",36,"bold"))
 times.pack()
