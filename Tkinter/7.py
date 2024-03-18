8a8560b8bd5d16626248b0590feacc16ceac0c5b
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-18 14:42:31 +0530

    button

--- Tkinter/7.py
+++ Tkinter/7.py
@@ -2,4 +2,10 @@ from tkinter import *
 root = Tk()
 root.geometry("1200x600")
 root.title("Buttons in Tkinter")
+
+frame1 = Frame(root,bg="grey",borderwidth=6,relief=SOLID)
+frame1.pack(side=LEFT,anchor="nw")
+
+
+
 root.mainloop()
