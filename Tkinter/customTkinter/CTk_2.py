3f1816cdb6ddff49dd7c6423096a9d9da2c9cbac
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-19 14:46:01 +0530

    ctk

---
+++ Tkinter/customTkinter/CTk_2.py
@@ -0,0 +1,22 @@
+from customtkinter import *
+from PIL import Image 
+
+root = CTk()
+root.geometry("700x700")
+set_appearance_mode("dark")
+
+label_1 = CTkLabel(master=root,text="RAM RAM LADD LE",text_color="crimson",font=("Arial",19))
+label_1.place(relx="0.5",rely="0.5",anchor="center")
+
+root.mainloop()
+
+
+# * What I have learnt
+# *
+# *
+# *
+# *
+# *
+# *
+# *
+# *
