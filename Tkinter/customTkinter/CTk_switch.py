aae15a372ed4e5beb8f543fafc04f1973fe2a1dd
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-19 15:14:45 +0530

    label

---
+++ Tkinter/customTkinter/CTk_switch.py
@@ -0,0 +1,23 @@
+from customtkinter import *
+from PIL import Image 
+
+root = CTk()
+root.geometry("700x700")
+set_appearance_mode("dark")
+
+
+Switch_widget = CTkSwitch(master=root,text="Are you 18+ ?")
+Switch_widget.place(relx="0.5",rely="0.5",anchor="center")
+
+root.mainloop()
+
+
+# * What I have learnt
+# * Switch
+# * 
+# *
+# *
+# *
+# *
+# *
+# *
