b3215b3a327b0469ed0a8e6c03cf0f2dc40c1944
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-20 23:10:30 +0530

    1st

--- Tkinter/New_way(ttk)/ttk1.py
+++ Tkinter/New_way(ttk)/ttk1.py
@@ -2,13 +2,38 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.geometry("400x200")
+root.geometry("400x140")
 root.title("Miles to Kilometer!")
 
-#&--------------    Heading       ------------------------------------------
+# ~  --------------------    Convert Function    --------------------------------------------------------------
+
+def  convert():
+    print(input_entry.get())
+
+# ~   ---------------------------------------------------------------------------------------------------------
+
+#&--------------    Heading (Label)     -----------------------------------------------------------------------
 
 title_label = ttk.Label(master=root,text="Miles to Kilometer Converter",font=("calibri",21,"bold")).pack()
 
+# & --------------------------------------------------------------------------------------------------------
+
+# ^ -----------------   ENTRY       ------------------------------------------------------------------------------
+input_frame = ttk.Frame(master=root )
+input_entry =  ttk.Entry(master=input_frame)
+input_button = ttk.Button(master=input_frame,text="Convert",command=convert)
+input_entry.pack(side="left",padx=10)
+input_button.pack(side="left")
+input_frame.pack(pady=10)
+
+# ^ --------------------------------------------------------------------------------------------------------------
+
+#!  ------------------- Output Label ------------------------------------------------------------------------------
+
+output_label = ttk.Label(master=root,text="Output", font=("Arial",16,"normal"))
+output_label.pack(pady=5)
+
+#!  ---------------------------------------------------------------------------------------------------------------
 
 root.mainloop()
 
