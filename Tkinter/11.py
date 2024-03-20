24c68ca646e964d33a5178949ffede7907dc111f
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-20 21:57:57 +0530

    11 21

--- Tkinter/11.py
+++ Tkinter/11.py
@@ -3,10 +3,15 @@ from PIL import Image , ImageTk
 
 root = Tk()
 
-root.geometry("1300x1200")
+root.geometry("700x700")
+
+def harry(event):
+    print(f"Clicked {event.x} & {event.y}")
 
 widget1 = Button(root,text="Click Me!")
 widget1.grid(row=1,column=4)
+
+widget1.bind('<Button-1>',harry)        #^  (Event,function)
 
 
 
--- Tkinter/12.py
+++ Tkinter/12.py
@@ -5,10 +5,24 @@ root = Tk()
 root.title("NewsPaper")
 root.geometry("700x700")
 
+texts=[]
+photos=[]
+for i in range(0,3):
+    with open (f"{i+1}.txt") as f:
+        text = f.read()
+        texts.append(text)
 
+    image = Image.open(f"{i+1}.png")
+    # ^ Resizing the images
+    photos.append(ImageTk.PhotoImage(image))
 
+f0 = Frame(root,height=70,width=800)
+Label(f0,text="Times of World",font=("lucida",36,"bold")).pack()
+Label(f0,text="JANUARY 19 2050",font=("lucida",16,"bold")).pack()
 
+f0.pack()
 
-
-
+f1 = Frame(root,width=800,height=200)
+Label(f1,text=texts[0]).pack()
+f1.pack()
 root.mainloop()
