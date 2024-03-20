af1f2a8188136fca8b1de97a739d58883b6bd686
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-20 22:38:24 +0530

    ttk

--- Tkinter/New_way(ttk)/ttk1.py
+++ Tkinter/New_way(ttk)/ttk1.py
@@ -1,3 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 
+root = tk.Tk()
+root.geometry("400x200")
+root.title("Miles to Kilometer!")
+
+#&--------------    Heading       ------------------------------------------
+
+title_label = ttk.Label(master=root,text="Miles to Kilometer Converter",font=("calibri",21,"bold")).pack()
+
+
+root.mainloop()
+
