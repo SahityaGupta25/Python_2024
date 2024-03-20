d66745ab6d7530f7f3beeda1c76cf961321cac64
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-20 14:42:10 +0530

    event

---
+++ Tkinter/11.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from PIL import Image , ImageTk
+
+root = Tk()
+
+root.geometry("1300x1200")
+
+widget1 = Button(root,text="Click Me!")
+widget1.grid(row=1,column=4)
+
+
+
+root.mainloop()
