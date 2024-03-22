baffe27590023a7387e5c5e08e7546127ceb02a0
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-22 17:43:19 +0530

    ttk2

--- Tkinter/New_way(ttk)/ttk2.py
+++ Tkinter/New_way(ttk)/ttk2.py
@@ -1,8 +1,22 @@
 import tkinter as tk
+from tkinter import ttk 
 
 window = tk.Tk()
+window.title("Window & Widgets")
 window.geometry("800x600")
 
+# ^ -----------  ttk text -------------
 text_1 = tk.Text(master=window)
 text_1.pack()
+
+
+# ^ -----------  ttk label -------------
+label_1 = ttk.Label(master=window,text="This is a test")
+label_1.pack()
+
+
+
+
+
+
 window.mainloop()
