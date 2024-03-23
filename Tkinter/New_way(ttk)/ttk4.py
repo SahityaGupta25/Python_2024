6d9d743f0e650a2d2db26cdb5762f37edaebf02c
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-23 09:05:37 +0530

    ttk4

--- Tkinter/New_way(ttk)/ttk4.py
+++ Tkinter/New_way(ttk)/ttk4.py
@@ -5,9 +5,15 @@ from tkinter import ttk
 root = tk.Tk()
 root.title("Buttons in tkinter (Time - 1:06:50)")
 
+str_var = tk.StringVar(value="Btn.ttk()")
 
-btn_ttk = ttk.Button(master=root,text="Click Me!",command=lambda:print("Hey its me")).pack()
+btn_ttk = ttk.Button(master=root,text="Click Me!",command=lambda:print("Hey its me"),textvariable=str_var).pack()
 
+def check_kar():
+    print(check_var.get())
+
+check_var = tk.IntVar()
+check_bttn= ttk.Checkbutton(master=root,text="Check box Demo",variable=check_var,command=check_kar).pack()
 
 root.geometry("800x400")
 
