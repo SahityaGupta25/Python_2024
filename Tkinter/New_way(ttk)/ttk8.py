6b90846ea418c6694792412d58fbae887d365da9
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-28 21:26:56 +0530

    8th

--- Tkinter/New_way(ttk)/ttk8.py
+++ Tkinter/New_way(ttk)/ttk8.py
@@ -3,8 +3,14 @@ from tkinter import ttk
 
 
 root = tk.Tk()
-root.title("Buttons in tkinter (Time - 1:06:50)")
+root.title("MENU (Time - 3:31:00)")
 
+
+menu = tk.Menu(master=root)
+fileMenu = tk.Menu(master=menu,tearoff=False)
+fileMenu.add_command(label='New', command= lambda:print('New file') )
+menu.add_cascade(label='File',menu=fileMenu)
+root.configure(menu=menu)
 
 
 root.geometry("800x400")
@@ -24,3 +30,16 @@ root.geometry("800x400")
 
 
 root.mainloop()
+
+# * -----------------   NOTES       ------------------------------------------------------------------------------
+
+# * Create a variable using "tk.menu()"................. menu = tk.Menu(master=root)
+# * Then create file menu variable with again "tk.menu()".............. fileMenu = tk.Menu(master=root,tearoff=False)
+# * Then write this............. menu.add_cascade(label='File',menu=fileMenu)
+# * Then to create a Sub Menu write this 
+# * root.configure(menu=menu)
+# *
+# *
+# *
+# *
+# *
