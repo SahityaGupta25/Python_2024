22ba22dec2bd643c80a70420d7a30936763825b8
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-30 21:12:16 +0530

    python

--- Tkinter/ttkbootstrap/Moder_Labels&button.py
+++ Tkinter/ttkbootstrap/Moder_Labels&button.py
@@ -4,12 +4,13 @@ import ttkbootstrap as tb
 from ttkbootstrap.constants import *
 
 
-root = tk.Tk()
+# root = tk.Tk()
+root = tb.Window(themename='superhero')
 root.title("Modern Labels and buttons")
 root.geometry("800x400")
 
-
-
+label_one = tb.Label(root,text="Hello!",font=("Arial",66),bootstyle = 'danger inverse')
+label_one.pack()
 root.mainloop()
 
 # * Instead of tk.Tk() use root = tb.window()
