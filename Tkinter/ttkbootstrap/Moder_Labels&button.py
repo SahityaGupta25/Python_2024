b59afc3dd5ca3e80708168cb3bd2238e8e7c364f
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-03-30 21:33:23 +0530

    btn

--- Tkinter/ttkbootstrap/Moder_Labels&button.py
+++ Tkinter/ttkbootstrap/Moder_Labels&button.py
@@ -10,9 +10,23 @@ root.title("Modern Labels and buttons")
 root.geometry("800x400")
 
 label_one = tb.Label(root,text="Hello!",font=("Arial",66),bootstyle = 'danger inverse')
-label_one.pack()
-root.mainloop()
+label_one.pack(pady=50)
 
+count = 0
+def changeColor():
+    global count
+    count +=1 
+    if count %2 ==0:
+        label_one.config(text="Hare Ram")
+    else:
+        label_one.config(text="Sita Ram")
+
+
+btn = tb.Button(root,text="Click Me!",bootstyle = 'success, outline',command=changeColor)
+btn.pack()
+
+
+root.mainloop()
 # * Instead of tk.Tk() use root = tb.window()
 # * we can add theme by writing root = tb.window(themename = theme_name)
 # *
