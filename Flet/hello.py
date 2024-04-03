32b088a27dea71451abdee36f2d81cd520cbecf5
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-04-03 17:04:32 +0530

    hello

--- Flet/hello.py
+++ Flet/hello.py
@@ -1,2 +1,22 @@
+# Importing the library
 import flet as ft
+
+# ! Defining the main function
+def main(page:ft.Page):
+    
+    # * Page Title
+    page.title="Hello World!"
+
+    # * Text
+    text = ft.Text(value='My first flet app',color='purple',bgcolor="yellow")
+
+    # * Appending & Updating to the page
+    page.controls.append(text)
+    page.update()
+
+
+# ^ Starting the App
+ft.app(target=main,view=ft.WEB_BROWSER)
+
+
 
