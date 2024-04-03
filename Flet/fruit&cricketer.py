d56875a94792f09da16717c3be2e7711a1e65ccc
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-04-03 19:08:28 +0530

    add

--- Flet/fruit&cricketer.py
+++ Flet/fruit&cricketer.py
@@ -1,7 +1,24 @@
 import flet as ft
 def main(page = ft.Page):
     page.title = "Fruits & Cricketers"
-    page.add(ft.Row(controls=[ft.Text("My favorite fruits :\n")]))
+    page.add(
+        ft.Row(controls=[ft.Text("My favorite fruits :\n")]),
+             ft.Row(
+                 controls=[
+                     ft.Text("Apple"),
+                     ft.Text("Papaya"),
+                     ft.Text("Mango")
+                 ]
+             ),
+        ft.Column(controls=[ft.Text("My favorite Cricketer:\n")]),
+             ft.Column(
+                 controls=[
+                     ft.Text("Sachin"),
+                     ft.Text("Virat"),
+                     ft.Text("Dhoni")
+                 ]
+             )
+    )
     #! page.update()
 
 ft.app(target=main)
