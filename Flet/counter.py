4f99c4804da13097e170c5ddb66d2f1f8f07c9ba
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-04-03 17:58:28 +0530

    counter

--- Flet/counter.py
+++ Flet/counter.py
@@ -1,7 +1,13 @@
 import flet as ft
+from time import sleep
 
 def main(page:ft.Page):
-    pass
+    page.title="Counter App - build with 'FLET'"
+    text = ft.Text()
+    for i in range(1,999999999+1):
+        text.value = "Count"+" "+str(i)
+        page.update()
+        sleep(1) # * It will make the program sleep for 1 second
 
 
 ft.app(target=main)
