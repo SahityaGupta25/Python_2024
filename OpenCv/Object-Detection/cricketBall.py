e4af6b327457d402a7e05957f62072e85d5f4d7b
Sahitya Gupta <99082422+SahityaGupta25@users.noreply.github.com>
2024-04-10 13:07:53 +0530

    cricket

--- OpenCv/Object-Detection/cricketBall.py
+++ OpenCv/Object-Detection/cricketBall.py
@@ -1,9 +1,9 @@
 import cv2
-vidcap = cv2.VideoCapture('../Videos/one_cricket_ball.mp4')
+vidcap = cv2.VideoCapture('C:/Users/vasug/OneDrive/Desktop/Python_2024/OpenCv/Object-Detection/Videos/one_cricket_ball.mp4')
 true , image = vidcap.read()
 
 count = 0
 while vidcap.isOpened():
     true,image = vidcap.read()
-    cv2.imwrite("C:\Users\vasug\OneDrive\Desktop\Python_2024\OpenCv\Object-Detection\Data Set (Videos)\Cricket_ball_one\%d.png" %count,image)
+    cv2.imwrite("C:/Users/vasug/OneDrive/Desktop/Python_2024/OpenCv/Object-Detection/Data Set (Videos)/Cricket_ball_one/%d.png" % count,image)
     count+=1
